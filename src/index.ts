f7de9d9faea6dc3214b53b82da6e7b205b23f454
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-01-11 15:06:27 +0700

    Using express router

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('hi');
+import express, { Request, Response } from 'express';
+import { router } from './ruotes/loginRoutes';
+
+const app = express();
+
+app.use(router);
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
