70d1314848d77c323beee2179b0461cad90bf153
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-01-11 15:53:42 +0700

    Login route

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from 'express';
-import { router } from './ruotes/loginRoutes';
+import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
 
 const app = express();
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 
 app.listen(3000, () => {
