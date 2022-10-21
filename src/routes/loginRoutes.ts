b9aa752d48bf4cf9a4f7ab1d4c1b779c180000cd
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2022-10-21 17:07:03 +0700

    Update loginRoutes.ts

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -33,6 +33,24 @@ router.get('/', (req: Request, res: Response) => {
   }
 });
 
+router.get('/test', (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+      <div>
+        <div>You are logged in</div>
+        <a href="/logout">Logout</a>
+      </div>
+    `);
+  } else {
+    res.send(`
+      <div>
+        <div>You are not logged in</div>
+        <a href="/login">Login</a>
+      </div>
+    `);
+  }
+});
+
 router.get('/logout', (req: Request, res: Response) => {
   req.session = undefined;
   res.redirect('/');
